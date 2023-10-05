8e91637b30da9e4813c74dfeff3fb01627cdd58d
Sebastian Vansteenkiste <svansteenkiste@hchb.com>
2023-10-05 15:43:37 +0200

    Demostrating passing parameters on test

--- MeatyBlazorTest/MainLayoutTests.cs
+++ MeatyBlazorTest/MainLayoutTests.cs
@@ -5,12 +5,25 @@ namespace MeatyBlazorTest;
 public class MainLayoutTests : TestContext
 {
 	[Fact]
-	public void MainLayout_RendersOk_WithoutChildContent()
+	public void MainLayout_RendersOk_WhenNotGivenChildContent()
 	{
 		// Arrange
 		var cut = RenderComponent<MainLayout>();
 
 		// Assert that the main layout can be drawn without content being given
-		cut.MarkupMatches("<main></main>");
+		var mainComponent = cut.Find("main");
+		Assert.Equal(string.Empty, mainComponent.InnerHtml);
+	}
+
+	[Fact]
+	public void MainLayout_RendersOk_WhenGivenChildContent()
+	{
+		// Arrange
+		string childContent = "<p>Some text</p>";
+		var cut = RenderComponent<MainLayout>(parameters => parameters.Add(p => p.Body, childContent));
+
+		// Assert that the main layout can be drawn without content being given
+		var mainComponent = cut.Find("main");
+		Assert.Equal(childContent, mainComponent.InnerHtml);
 	}
 }
